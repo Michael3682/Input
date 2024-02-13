21119cefba6f697166545271fbf9b78af7266ad4
Michael3682 <152974233+Michael3682@users.noreply.github.com>
2024-02-13 18:55:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const ipt = document.getElementById('input')
+const opt = document.getElementById('output')
+const btn = document.getElementById('button')
+
+const output = () => {
+    let count = 1
+    
+    opt.classList.add('output2')
+    opt.innerText = ipt.value
+    
+    const timer = setInterval( () => {
+        count--
+        if (count == 0) {
+            clearInterval(timer);
+            opt.classList.remove('output2')
+        }
+        console.log(count)
+    }, 500);
+}
+
+btn.addEventListener('click', output)
